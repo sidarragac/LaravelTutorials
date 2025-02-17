e0eb2de8cfb6310e6be2c928d608396374e0ad2f
Santiago Idárraga Ceballos <san200526@gmail.com>
2025-02-16 23:01:57 -0500

    Added create link to the navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
           <a class="nav-link active" href="{{ route('home.index') }}">Home</a>
           <a class="nav-link active" href="{{ route('home.about') }}">About</a>
           <a class="nav-link active" href="{{ route('product.index') }}">Products</a>
+          <a class="nav-link active" href="{{ route('product.create') }}">Create a Product</a>
         </div>
       </div>
     </div>
