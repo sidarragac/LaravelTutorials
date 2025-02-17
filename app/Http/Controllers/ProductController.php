9a4807f753995ce3ecf914b8b31ebab77c36dfe7
Santiago Idárraga Ceballos <san200526@gmail.com>
2025-02-16 20:12:53 -0500

    Added and displayed price to products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,10 +8,10 @@ use Illuminate\View\View;
 class ProductController extends Controller{
 
     public static $products = [
-        ["id"=>"1", "name"=>"TV", "description"=>"Best TV"],
-        ["id"=>"2", "name"=>"iPhone", "description"=>"Best iPhone"],
-        ["id"=>"3", "name"=>"Chromecast", "description"=>"Best Chromecast"],
-        ["id"=>"4", "name"=>"Glasses", "description"=>"Best Glasses"]
+        ["id"=>"1", "name"=>"TV", "description"=>"Best TV", "price"=>1000],
+        ["id"=>"2", "name"=>"iPhone", "description"=>"Best iPhone", "price"=>1800],
+        ["id"=>"3", "name"=>"Chromecast", "description"=>"Best Chromecast", "price"=>50],
+        ["id"=>"4", "name"=>"Glasses", "description"=>"Best Glasses", "price"=>275]
     ];
 
 
