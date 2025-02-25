eb939430de7f9f326a2e7cd692d3904d2437ec46
Santiago Idarraga Ceballos <san200526@gmail.com>
2025-02-25 13:44:45 -0500

    Completed cart section with "cookies"

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Home Controller
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
 
 Route::get('/about', function () {
@@ -18,7 +19,13 @@ Route::get('/about', function () {
 
 Route::get('/contact', 'App\Http\Controllers\HomeController@contact')->name('home.contact');
 
+// Products Controller
 Route::get('/products', 'App\Http\Controllers\ProductController@index')->name('product.index');
 Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name('product.create');
 Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name('product.save');
 Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name('product.show');
+
+// Cart Controller
+Route::get('/cart', 'App\Http\Controllers\CartController@index')->name('cart.index');
+Route::get('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name('cart.add');
+Route::get('/cart/removeAll/', 'App\Http\Controllers\CartController@removeAll')->name('cart.removeAll');
