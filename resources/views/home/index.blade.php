396e13346e174c3c4f2483adc8315911d785f005
Santiago Idarraga Ceballos <san200526@gmail.com>
2025-03-11 09:51:13 -0500

    Added buttons to access to ninjas' section features

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,7 +3,21 @@
 @section('content')
 
 <div class="text-center">
-    Welcome to the application
+    <a href="{{ route('ninja.index') }}">
+        <button type="button" class="btn btn-primary">
+            View Ninjas
+        </button>
+    </a>
+    <a href="{{ route('ninja.create') }}">
+        <button type="button" class="btn btn-secondary">
+            Create a Ninja
+        </button>
+    </a>
+    <a href="{{ route('ninja.stats') }}">
+        <button type="button" class="btn btn-secondary">
+            View Ninjas' Stats
+        </button>
+    </a>
 </div>
 
 @endsection
