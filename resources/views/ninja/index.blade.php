be56bc60baefd26a032b77f2c606978e5338c8f5
Santiago Idarraga Ceballos <san200526@gmail.com>
2025-03-11 10:40:12 -0500

    Created view to list ninjas with given conditions

---
+++ resources/views/ninja/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+@section('title', $viewData["title"])
+@section('subtitle', $viewData["subtitle"])
+@section('content')
+
+<div class="row">
+    @foreach ($viewData["ninjas"] as $ninja)
+        <div class="col-md-4 col-lg-3 mb-2">
+            <div class="card">
+                <div class="card-header">
+                    {{ $ninja->getName() }}
+                </div>
+                <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                        <p>Id: {{ $ninja->getId() }}</p>
+                        <footer class="blockquote-footer">
+                            <cite title="Source Title">Village: {{ $ninja->getVillage() }}</cite>
+                            @if($ninja->getVillage() == "Hoja")
+                                <cite title="Source Title" style="color: red;">
+                                    Chakra: {{ $ninja->getChakra() }}
+                                </cite>
+                            @else
+                                <cite title="Source Title" style="color: blue;">
+                                    Chakra: {{ $ninja->getChakra() }}
+                                </cite>
+                            @endif
+                        </footer>
+                    </blockquote>
+                </div>
+                <div class="card-body text-center">
+                    <a 
+                        href="{{ route('ninja.show', ['id'=> $ninja->getId()]) }}" 
+                        class="btn bg-primary text-white"
+                    >
+                        + Info
+                    </a>
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
+@endsection
