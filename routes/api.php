94f90ef761fb5b81bc463295acfcd9b0464a2d61
Santiago Idárraga Ceballos <san200526@gmail.com>
2025-03-28 00:00:15 -0500

    API system with LARAVEL collections

--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,5 @@ Route::get('/products', 'App\Http\Controllers\Api\ProductApiController@index')->
 Route::get('/products/{id}', 'App\Http\Controllers\Api\ProductApiController@show')->name('api.product.show');
 Route::get('/v2/products', 'App\Http\Controllers\Api\ProductApiControllerV2@index')->name('api.v2.product.index');
 Route::get('/v2/products/{id}', 'App\Http\Controllers\Api\ProductApiControllerV2@show')->name('api.v2.product.show');
+Route::get('/v3/products', 'App\Http\Controllers\Api\ProductApiControllerV3@index')->name('api.v3.product.index');
+Route::get('/v3/products/paginate', 'App\Http\Controllers\Api\ProductApiControllerV3@paginate')->name('api.v3.product.paginate');
