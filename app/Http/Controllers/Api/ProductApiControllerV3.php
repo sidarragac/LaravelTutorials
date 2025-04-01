c4b969f17de65883770512334dfb27f44e265958
Santiago Idárraga Ceballos <san200526@gmail.com>
2025-03-31 20:40:22 -0500

    Created post functionality

--- app/Http/Controllers/Api/ProductApiControllerV3.php
+++ app/Http/Controllers/Api/ProductApiControllerV3.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductCollection;
 use App\Models\Product;
+use Illuminate\Http\Client\Request;
 use Illuminate\Http\JsonResponse;
 
 class ProductApiControllerV3 extends Controller
@@ -21,5 +22,19 @@ class ProductApiControllerV3 extends Controller
         $products = new ProductCollection(Product::paginate(5));
 
         return response()->json($products, 200);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $request->validate([
+            'name' => 'required',
+            'price' => 'required|numeric',
+        ]);
+        $product = Product::create(request()->only(['name', 'price']));
+
+        return response()->json([
+            'message' => 'Product created successfully!',
+            'product' => $product,
+        ], 201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -12,4 +12,5 @@ Route::get('/products/{id}', 'App\Http\Controllers\Api\ProductApiController@show
 Route::get('/v2/products', 'App\Http\Controllers\Api\ProductApiControllerV2@index')->name('api.v2.product.index');
 Route::get('/v2/products/{id}', 'App\Http\Controllers\Api\ProductApiControllerV2@show')->name('api.v2.product.show');
 Route::get('/v3/products', 'App\Http\Controllers\Api\ProductApiControllerV3@index')->name('api.v3.product.index');
+Route::post('/v3/products', 'App\Http\Controllers\Api\ProductApiController@store')->name('api.v3.product.store');
 Route::get('/v3/products/paginate', 'App\Http\Controllers\Api\ProductApiControllerV3@paginate')->name('api.v3.product.paginate');
